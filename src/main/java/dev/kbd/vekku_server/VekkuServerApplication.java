dae93b7c98c4c6b4149146584e98e0f7c2483494
kuchuk-borom-debbarma <kuchukboromd@gmail.com>
2025-12-14 16:41:00 +0530

    feat: add Docker Compose configuration

---
+++ src/main/java/dev/kbd/vekku_server/VekkuServerApplication.java
@@ -0,0 +1,13 @@
+package dev.kbd.vekku_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VekkuServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VekkuServerApplication.class, args);
+	}
+
+}
