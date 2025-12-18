5190d108607a52c12031cf23a6050123734c4c93
kuchuk-borom-debbarma <kuchukboromd@gmail.com>
2025-12-18 22:40:49 +0530

    [server-spring] feat: enable CORS for AuthController and configure allowed origins in properties

--- vekku-server/src/main/java/dev/kbd/vekku_server/controllers/AuthController.java
+++ vekku-server/src/main/java/dev/kbd/vekku_server/controllers/AuthController.java
@@ -5,6 +5,7 @@ import dev.kbd.vekku_server.dto.auth.VerifyOtpRequest;
 import dev.kbd.vekku_server.services.auth.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "${app.cors.allowed-origins}")
 public class AuthController {
 
     private final AuthService authService;
